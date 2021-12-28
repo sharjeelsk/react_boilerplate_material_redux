c43e946fd25a2fdb1f17a1e52f706661f7fabb0f
sharjeelsk <54668506+sharjeelsk@users.noreply.github.com>
2021-12-28 11:59:37 +0530

    boiler plate code

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header/Header'
+
+function Home() {
+    return (
+        <div>
+            <Header id="1" />
+            Home
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/redux/Store.js
@@ -0,0 +1,26 @@
+import {createStore,applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+import RootReducer from './RootReducer'
+import thunk from 'redux-thunk'
+import { persistStore,persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import storageSession from 'redux-persist/lib/storage/session'
+import { createTransform } from 'redux-persist';
+import JSOG from 'jsog'
+// export const JSOGTransform = createTransform(
+//     (inboundState, key) => JSOG.encode(inboundState),
+//     (outboundState, key) => JSOG.decode(outboundState),
+// )
+
+const persistConfig = {
+    key:'root',
+    storage:storageSession,
+    blackList:['socket']
+   // transforms: [JSOGTransform]
+}
+const persistedReducer = persistReducer(persistConfig,RootReducer)
+
+const middlewares = [logger,thunk];
+
+export const store = createStore(persistedReducer,applyMiddleware(...middlewares))
+export const Persister = persistStore(store)
